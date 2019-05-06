01cad48ed9ab88c137213e6d20ec7138fb59ec0b
obpumanee <obpumanee@gmail.com>
2019-05-06 11:54:39 +1000

    add first page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
     def index
+           # get current seller
+           @sellers = Seller.all
+           @product_listings = ProductListing.all 
     end
 end
+
+
+    
+
+
+  
